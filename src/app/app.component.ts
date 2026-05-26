bfb7b94094f1ac4fc8132d52e27f1c7d957d942a
Pierluigi Mastroianni <pierluigi-mastroianni@dedagroup.it>
2026-05-26 10:44:52 +0200

    Creazione navbar and carousel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cibando';
+
+  foods=[
+    {
+      id:1,
+      label:"Lasagna"
+    },
+    {
+      id:2,
+      label:"Arrosto con patate"
+    },
+     {
+      id:3,
+      label:"Tiramisù"
+    },
+      {
+      id:4,
+      label:"Piadina"
+    },
+  ];
+
+  pathFoodsPictures="../assets/images/image_";
+
 }
